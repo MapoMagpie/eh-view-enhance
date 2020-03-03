b1e9e7c2edb1511b78888027ea9d7bf140f6a8f6
kamo2020 <zsyjk@live.cn>
2020-03-04 00:39:38 +0800

    Update EH_VIEW_PLUS.js

--- EH_VIEW_PLUS.js
+++ EH_VIEW_PLUS.js
@@ -18,13 +18,13 @@ class ImgPlus {
     retry() {
         this.finsh = false;
         this.set();
-        this.fetchImg().then(src => { bigImageFrame.src = src });
+        this.fetchImg().then(src => { bigImageElement.src = src });
     }
 
     set() {
-        bigImageFrame.hidden = false;
-        bigImageFrame.src = this.bigImageUrl || this.node.src;
-        bigImageFrame.setAttribute("index", this.index);
+        bigImageFrame.style.display = "flex";
+        bigImageElement.src = this.bigImageUrl || this.node.src;
+        bigImageElement.setAttribute("index", this.index);
     }
 }
 
@@ -46,31 +46,39 @@ let styleSheel = document.createElement("style");
 styleSheel.textContent =
     `
     .fullViewPlane {
-        width: 100%; 
-        height: 100%; 
-        background-color: #ff71f01c;
+        width: 100%;
+        height: 100%;
+        background-color: #000;
         position: fixed;
         top: 0px;
         right: 0px;
         z-index: 1000;
         display: flex;
         flex-wrap: wrap;
-        justify-content: center;
+        justify-content: flex-start;
+        overflow: scroll;
     }
 
     .fullViewPlane > img:not(.bigImageFrame) {
-        margin: 10px 0px 0px 10px;
+        margin: 20px 0px 0px 22px;
         width: 10%;
+        border: 5px
+        gray solid;
+        box-sizing: border-box;
+        height: max-content;
     }
 
     .bigImageFrame {
-        top: 20px; 
-        display: block; 
-        position: absolute; 
-        left: auto; 
-        width: auto;
-        height: 100%;
+        position: fixed;
+        width: 100%;
         z-index: 1001;
+        background-color: #000000d6;
+        justify-content: center;
+        height: 100%;
+    }
+
+    .bigImageFrame > img {
+        height: 100%;
     }
 `;
 
@@ -79,40 +87,82 @@ document.head.appendChild(styleSheel);
 let fullViewPlane = document.createElement("div");
 fullViewPlane.classList.add("fullViewPlane");
 
-let bigImageFrame = document.createElement("img");
+let bigImageFrame = document.createElement("div");
 bigImageFrame.classList.add("bigImageFrame");
-bigImageFrame.hidden = true;
+bigImageFrame.style.display = "none";
 fullViewPlane.appendChild(bigImageFrame);
-bigImageFrame.addEventListener("wheel", (event) => {
+let bigImageElement = document.createElement("img");
+bigImageFrame.appendChild(bigImageElement);
+bigImageElement.addEventListener("wheel", (event) => {
     let start = parseInt(event.target.getAttribute("index"));
     imgPlusList.do(start, 5);
     if (event.deltaY > 0) {
-        if (start > imgPlusList.length) return;
+        if (start > imgPlusList.length - 2) return;
         imgPlusList[start + 1].set();
     } else {
         if (start - 1 < 0) return;
         imgPlusList[start - 1].set();
     }
-})
-
-let source = document.querySelector("#gdt");
-
-let imgPlusList = new ImgPlusList();
-
-[].slice.call(source.childNodes).forEach((node, index) => {
-    if (node.nodeType === 1 && node.hasChildNodes()) {
-        let aE = node.firstChild;
-        let imgE = aE.firstChild.cloneNode(true);
-        imgE.setAttribute("index", index);
-        fullViewPlane.appendChild(imgE);
-        imgPlusList.push(new ImgPlus(aE.getAttribute("href"), imgE, index));
-        imgE.addEventListener("click", (event) => {
-            let start = parseInt(event.target.getAttribute("index"));
-            imgPlusList[start].retry();
-            imgPlusList.do(start, 5);
-        })
-    }
+    event.stopPropagation();
 });
 
+bigImageFrame.addEventListener("click", (event) => { if (event.target.tagName === "IMG") return; bigImageFrame.style.display = "none"; })
+
 document.body.appendChild(fullViewPlane);
+let imgPlusList = new ImgPlusList();
+
+let putImgPlus = function (imgEleList, imgPlusList, fullViewPlane) {
+    [].slice.call(imgEleList.childNodes).forEach((node) => {
+        if (node.nodeType === 1 && node.hasChildNodes()) {
+            let aE = node.firstChild;
+            let imgE = aE.firstChild.cloneNode(true);
+            fullViewPlane.appendChild(imgE);
+            let index = imgPlusList.push(new ImgPlus(aE.getAttribute("href"), imgE, imgPlusList.length)) - 1;
+            imgE.setAttribute("index", index);
+            imgE.addEventListener("click", (event) => {
+                let start = parseInt(event.target.getAttribute("index"));
+                imgPlusList[start].retry();
+                imgPlusList.do(start, 5);
+            });
+        }
+    });
+}
+
+
+let fetchSource = async function (href) {
+    const response = await window.fetch(href);
+    const text = await response.text();
+    let ele = document.createElement("div");
+    ele.innerHTML = text;
+    return ele;
+}
+
+let nextPage = async function (source) {
+    let e1 = source.querySelector("table.ptb td.ptds");
+    if (!e1) return null;
+    let e2 = e1.nextElementSibling;
+    if (!e2 || e2.textContent === ">") return null;
+    return e2.firstElementChild.href;
+}
+
+let processCurrPage = async function (href) {
+    const source = await fetchSource(href);
+    putImgPlus(source.querySelector("#gdt"), imgPlusList, fullViewPlane);
+    const nextHref = await nextPage(source);
+    return nextHref ? processCurrPage(nextHref) : true;
+}
+
+processCurrPage(document.querySelectorAll("table.ptb tr > td")[1].firstElementChild.href);
+/* let currPage = 0;
+let hrefs = [].slice.call(document.querySelectorAll("table.ptb tr > td"), 1).map((node, index) => { if (node.className === "ptds") currPage = index; return node.firstElementChild.href }); hrefs.pop();
+hrefs.forEach((href, index) => {
+     if (currPage === index) {
+        putImgPlus(document.querySelector("#gdt"), imgPlusList, fullViewPlane)
+    } else {
+        fetchSource(href).then(source => { putImgPlus(source, imgPlusList, fullViewPlane); });
+    }
+    fetchSource(href).then(source => { putImgPlus(source, imgPlusList, fullViewPlane); });
+}) */
+
+/* document.body.appendChild(fullViewPlane); */
 

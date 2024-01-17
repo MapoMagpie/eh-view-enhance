cd20a423ab086730a57cef4b5a68f34b0bf42728
MapoMagpie <zsyjk@live.cn>
2024-01-17 18:27:34 +0800

    modify version to 4.1.16

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
           'https://www.pixiv.net/*',
         ],
         name: { "": "E HENTAI VIEW ENHANCE", "zh-CN": "E绅士阅读强化" },
-        version: '4.1.15',
+        version: '4.1.16',
         license: 'MIT',
         author: 'MapoMagpie',
         description: {

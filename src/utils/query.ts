9b929157f9f7709466445868cafe5de7594b9da3
MapoMagpie <zsyjk@live.cn>
2024-02-05 15:17:11 +0800

    try keep more image cache;

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -19,18 +19,21 @@ export function xhrWapper<T extends RespType>(url: string, respType: T, cb: Even
     url,
     timeout: conf.timeout * 1000,
     responseType: respType,
+    nocache: false,
+    revalidate: false,
+    // fetch: false,
     headers: {
       "Host": HOST_REGEX.exec(url)![1],
       // "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
       "Accept": "image/avif,image/webp,*/*",
       // "Accept-Language": "en-US,en;q=0.5",
       // "Accept-Encoding": "gzip, deflate, br",
-      "Connection": "keep-alive",
+      // "Connection": "keep-alive",
       "Referer": window.location.href,
       // "Sec-Fetch-Dest": "image",
       // "Sec-Fetch-Mode": "no-cors",
       // "Sec-Fetch-Site": "cross-site",
-      "Cache-Control": "public,max-age=3600,immutable",
+      "Cache-Control": "public, max-age=2592000, immutable",
     },
     ...cb
   });

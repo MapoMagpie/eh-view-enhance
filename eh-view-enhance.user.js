bd04d23dc5618b9f0fa07fc4c4671f442b7153bf
MapoMagpie <zsyjk@live.cn>
2024-09-22 14:33:31 +0800

    chore: modify version to 4.9.1;

--- eh-view-enhance.user.js
+++ eh-view-enhance.user.js
@@ -6,7 +6,7 @@
 // @name:ko            만화 베틀
 // @name:ru            Комические ткацкие станки
 // @namespace          https://github.com/MapoMagpie/eh-view-enhance
-// @version            4.9.0
+// @version            4.9.1
 // @author             MapoMagpie
 // @description        Manga Viewer + Downloader, Focus on experience and low load on the site. Support: e-hentai.org | exhentai.org | pixiv.net | 18comic.vip | nhentai.net | hitomi.la | rule34.xxx | danbooru.donmai.us | gelbooru.com | twitter.com | wnacg.com | manhuagui.com | mangacopy.com | yande.re | hentainexus.com | koharu.to | arca.live
 // @description:zh-CN  漫画阅读 + 下载器，注重体验和对站点的负载控制。支持：Support: e-hentai.org | exhentai.org | pixiv.net | 18comic.vip | nhentai.net | hitomi.la | rule34.xxx | danbooru.donmai.us | gelbooru.com | twitter.com | wnacg.com | manhuagui.com | mangacopy.com | yande.re | hentainexus.com | koharu.to | arca.live

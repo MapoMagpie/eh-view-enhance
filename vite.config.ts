02f3e1d4b14e4889d90d6b3c1ee53d120ea3240b
MapoMagpie <zsyjk@live.cn>
2024-12-23 14:18:11 +0800

    chore: modify version to 4.10.3;

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import monkey, { cdn } from 'vite-plugin-monkey';
 
-const VERSION = '4.10.2';
+const VERSION = '4.10.3';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   let downloadURL: string | undefined;
